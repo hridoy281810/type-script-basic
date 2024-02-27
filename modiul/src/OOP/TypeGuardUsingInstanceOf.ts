d0d5fe46f89388d000943042f072360061128ee9
hridoy281810 <anisurrahman01815001904@gmail.com>
2024-02-27 11:32:05 +0600

    use instanceof

--- modiul/src/OOP/TypeGuardUsingInstanceOf.ts
+++ modiul/src/OOP/TypeGuardUsingInstanceOf.ts
@@ -1,6 +1,6 @@
 {
     // 
-// type gard with interface 
+// type gard with instanceof 
 class Animal {
     name: string
     species: string 
@@ -24,11 +24,31 @@ class Cat extends Animal {
     constructor(name: string,species:string){
         super(name,species)
     }
-    makeMeawe(){
+    makeMeaw(){
 console.log(`i am meaw`)
     }
 }
 
+// to handle smart way 
+const isDog = (animal: Animal): animal is Dog=>{
+    return animal instanceof Dog
+}
+const isCat = (animal: Animal): animal is Cat=>{
+    return animal instanceof Cat
+}
+const getAnimal = (animal:Animal)=>{
+    animal.makeSound()
+    if(isDog(animal)){
+        animal.makeBark()
+    }else if(isCat(animal)){
+        animal.makeMeaw()
+    }else{
+        animal.makeSound()
+    }
 
+}
+const dog = new Dog("Dog Bhai", "dog")
+const cat = new Cat("Cat Bhai", "cat")
+getAnimal(cat)
 // 
 }
